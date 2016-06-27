6d79e22c8ea459bb7d0dcb03798b80468bb92aa9
Michelle Sri <michelle.sri@gmail.com>
2016-06-27 15:35:18 -0700

    just keeping track of work ms

--- sandbox-censusDataAPI/app.js
+++ sandbox-censusDataAPI/app.js
@@ -2,6 +2,9 @@ var sdk = new CitySDK(); // create the CitySDK Instance
 census = sdk.modules.census; // Create an instance of the module
 census.enable('ebfc2b4e08bb16ebc53b99604e465907d6d087d5'); // enable the module with the api key
 
+var stateArray = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY'
+];
+
 function populateArr() {
   for (var i = 0; i < 4; i++) {
     requestArr.push(request + 'i');
@@ -11,29 +14,17 @@ function populateArr() {
 var requestArr = [
   {
     'level':'state',
-    'state':'CA',
+    'state':'',
     'variables': [
-      'age',
-      'commute_time'
-    ]
-  },
-  {
-    'level':'state',
-    'state':'OR',
-    'variables': [
-      'age',
-      'commute_time'
-    ]
-  },
-  {
-    'level':'state',
-    'state':'AZ',
-    'variables': [
-      'age',
-      'commute_time'
-    ]
-  }
-];
+      'income',
+      'median_home_value'
+    ],
+    api:'asc5',
+    year: '2014'
+  };
+
+  // write a function taht returns the median income and median home value for every county in the usa
+
 
 var callback = function(response) {
   $('#blah').append('<p>' + response.data[0]['commute_time'] + '</p>').text;
