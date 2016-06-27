b975eab6f3cec7e379b961798fbc079c0a2e9dd9
Lizzie Gooding <gooding.lizzie@gmail.com>
2016-06-27 13:46:04 -0700

    Wrote properties to JSON object, affordability calculations functional

--- sandbox-mapbox/model.js
+++ sandbox-mapbox/model.js
@@ -1,12 +1,31 @@
 //Calculate annual and montly housing costs based on 20% down (no PMI)
-var testGeojson = JSON.parse( 'County_MHV.geojson');
-console.log(testGeojson.type);
-var interestRate = 0.045;
-var salePrice = 300000;
-var mMortgage = ((interestRate / 12) * (salePrice * 0.8)) / (1 - ((1 + ( interestRate / 12)) ^ (-30 * 12)));
-var mInsurance = salePrice / 1000 * 3.5 / 12;
-var mUtilities = 250;
-// var mPMI = salePrice - (mMortgage * 12) * 0.01;
-var mPropertyTax = salePrice * 0.5983 * 0.02 / 12;
-var mPayment = mMortgage + mInsurance + mUtilities + mPropertyTax;
-var aPayment = mPayment * 12;
+function fetchMHV(callback){
+  $.getJSON('County_MHV.geojson')
+  .done(function(data){
+    console.log('Success!');
+    testGeojson = data;
+    console.log(testGeojson);
+    callback();
+  })
+  .fail(function() { console.log('Problem with data!'); })
+  .always(function() { console.log('Try to get JSON data from server.');
+  });
+}
+
+function calcAffordability(data){
+  var interestRate = 0.045;
+  // var mPMI = salePrice - (mMortgage * 12) * 0.01;
+  testGeojson.features.forEach(function(ele){
+    var salePrice = ele.properties.Median_Hom;
+    ele.properties.mMortgage = ((interestRate / 12) * (salePrice * 0.8)) / (1 - ((1 + ( interestRate / 12)) ^ (-30 * 12)));
+    ele.properties.mInsurance = salePrice / 1000 * 3.5 / 12;
+    ele.properties.mUtilities = 250;
+    ele.properties.mPropertyTax = salePrice * 0.5983 * 0.02 / 12;
+    ele.properties.mPayment = ele.properties.mMortgage + ele.properties.mInsurance + ele.properties.mUtilities + ele.properties.mPropertyTax;
+    ele.properties.aPayment = ele.properties.mPayment * 12;
+  });
+  // console.log(data);
+  return data;
+}
+
+var myJSON = JSON.stringify(fetchMHV(calcAffordability));
