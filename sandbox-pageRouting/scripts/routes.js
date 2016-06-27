c55473a4329825e89ea87681efc5dfb58fa17ed7
mericks <mericks@users.noreply.github.com>
2016-06-27 16:33:15 -0700

    created function to set route mapping

---
+++ sandbox-pageRouting/scripts/routes.js
@@ -0,0 +1,11 @@
+function setRouteMappings() {
+  page.base('/');
+
+  page('home', homeController.index);
+  page('compare', compareController.index);
+  page('afford', affordController.index);
+
+  page();
+}
+
+setRouteMappings();
