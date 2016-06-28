6a8478861f48695cab834241276f1e237b831c0a
mericks <mericks@users.noreply.github.com>
2016-06-28 11:28:51 -0700

    Fix #43 - debuged: works with pushstate server; added in 404 page for good measure

--- js/routes.js
+++ js/routes.js
@@ -6,6 +6,9 @@ function setRouteMappings() {
   page('compare', compareController.index);
   page('afford', affordController.index);
   page('about', aboutController.index);
+  page('*', function() {
+    $('body').html('Sorry! The page you requested doesn\'t exist. Hit your browser\'s "back" button to return to the previous page');
+  });
 
   page('index.html', '/');
 
