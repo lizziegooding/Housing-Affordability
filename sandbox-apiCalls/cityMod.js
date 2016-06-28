e4b05f298664aae9004bc0d794d8be2fe9222ee1
Aaron Bini <bini.aaron@gmail.com>
2016-06-27 23:33:51 -0700

    Fix #23

--- sandbox-apiCalls/cityMod.js
+++ sandbox-apiCalls/cityMod.js
@@ -1,34 +1,16 @@
-var stateArray = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY'];
+var national = {};
+
+national.countyHomes = [];
+
+national.nationData = [];
+
+national.stateArray = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY'];
 
 var sdk = new CitySDK();
 var censusModule = sdk.modules.census;
 censusModule.enable('34c1995fd27b0768996406c90a11d8ddf0024344');
 
-// var request2 = {
-//
-//     "level": "state",
-//     "state": "OR",
-//     "sublevel": true,
-//     "variables": [
-//         "median_home_value",
-//         'income',
-//     ],
-//     "api": "acs5",
-//     "year": "2014"
-//
-// };
-
-var request = {
-  state: 'AK',
-  level: 'level',
-  sublevel: true,
-  variables: [
-    'median_home_value',
-    'income',
-  ],
-  api: 'acs5',
-  year: '2014'
-};
+//the below is functional for adding geojson to a google map
 
 // $('button').on('click', function(){
 //   censusModule.GEORequest(request4, function(response) {
@@ -37,46 +19,75 @@ var request = {
 //   });
 // });
 
-$('button').on('click', function(){
-  var request = {
-    state: 'AK',
-    level: 'level',
-    sublevel: true,
-    variables: [
-      'median_home_value',
-      'income',
-    ],
-    api: 'acs5',
-    year: '2014'
-  };
-  var requestsArray = [];
-  stateArray.forEach(function(element){
-    request4.state = element;
-    requestsArray.push(censusModule.APIRequest(request4, function(response){
-      return response;
-    }));
-    return requestsArray;
-  });
-  var defer = $.when.apply($, requestsArray);
-  defer.done(function(){
-    $.each(arguments, function(index, data){
-      console.log(data);
+var request = {
+  state: '',
+  level: 'state',
+  sublevel: true,
+  variables: [
+    'median_home_value',
+    'income',
+    'median_gross_rent'
+  ],
+  api: 'acs5',
+  year: '2014'
+};
+
+national.nationalMedian = function(viewFunc) {
+  var counter = 0;
+  national.stateArray.forEach(function(element){
+    request.state = element;
+    censusModule.APIRequest(request, function(response){
+      national.nationData.push(response.data);
+      counter += 1;
+      if (counter === national.stateArray.length) {
+        viewFunc();
+      }
     });
   });
+};
+
+national.concatArray = function(array) {
+  var merged = [].concat.apply([], array);
+  return merged;
+};
+
+//this could be used as blanket function for filtering based on whatever data you want
+//category would be whatever national category you want to isolateData from
+//like national.buildCountyHomes but instead of explicitly passing median_home_value, that would be category parameter
+national.isolateData = function(category){};
+
+national.buildCountyHomes = function () {
+  var countiesArray = national.concatArray(national.nationData);
+  national.countyHomes = countiesArray.map(function(element){
+    return element.median_home_value;
+  });
+};
+
+national.whereCanIBuy = function (array, value1, value2, value3) {
+  var interestRate = 0.045;
+  var requiredMonthly = array.map(function(element){
+    var monthlyPayment = {};
+    var homePrice = element - value3;     /* (i x A) / (1 - (1 + i)^-N) */
+    monthlyPayment.mMortgage = ((interestRate / 12) * homePrice) / (1 - Math.pow((1 + (interestRate / 12)),(-30 * 12)));
+    monthlyPayment.mInsurance = (homePrice / 1000 * 3.5 / 12);
+    monthlyPayment.mUtilities = 250;
+    monthlyPayment.mPropertyTax = homePrice * 0.5983 * 0.02 / 12;
+    monthlyPayment.mPayment = monthlyPayment.mMortgage + monthlyPayment.mInsurance + monthlyPayment.mUtilities + monthlyPayment.mPropertyTax;
+    monthlyPayment.aPayment = monthlyPayment.mPayment * 12;
+    return monthlyPayment;
+  });
+  var accessibleCounties = requiredMonthly.filter(function(element){
+    return element.mPayment < (value1*value2);
+  });
+  // Math.round(num * 100) / 100
+  return '<p>In ' + Math.round(((accessibleCounties.length)/(national.countyHomes.length)*100)*100) / 100 + '% of counties in the US</p>';
+};
+
+$('#button1').on('click', function(){
+  national.buildCountyHomes();
+  $('#percentHomes').append(national.whereCanIBuy(national.countyHomes, $('#income').val(), $('#incomePercent').val(), $('#downP').val()));
 });
 
-// var defer = $.when.apply($, jqXHRs); defer.then(function() { ... });
-
-// var stringed;
-// censusModule.GEORequest(request3, function(response) {
-//   var county = {};
-//   county.type = 'geojson';
-//   county['data'] = {};
-//   county['data']['type'] = 'Feature';
-//   county['data']['geometry'] = response.features[0].geometry;
-//   county['data']['properties'] = response.features[0].properties;
-//   stringed = JSON.stringify(county.data);
-//   console.log(response);
-// });
-//
-// console.log(data);
+$(function(){
+  national.nationalMedian(national.buildCountyHomes);
+});
--- sandbox-apiCalls/zillow.js
+++ sandbox-apiCalls/zillow.js
@@ -1,22 +1,59 @@
-// var zillow = {};
-// zillow.id = 'X1-ZWz19oh35h56h7_2jayc';
+var zillow = {};
+
+zillow.id = 'X1-ZWz1fc2p5p72ff_3fm17';
+
+zillow.statesFull = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY'];
+
+zillow.states = ['OR', 'WA', 'PA', 'CA'];
+
 // zillow.state = 'OR';
-// zillow.datas = [];
-//
-// zillow.requestData = function() {
-//   $.ajax({
-//     url: ' https://www.zillow.com/webservice/GetRegionChildren.htm?zws_id=' + zillow.id + '&state=' + zillow.state + '&childtype=county',
-//     type: 'GET',
-//     // headers: { 'Authorization': 'token ' + githubToken },
-//     success: function(data, message, xhr) {
-//       zillow.datas = data;
-//       console.log(zillow.datas);
-//     }
-//   });
-// };
-//
+zillow.datas = [];
+
+zillow.requestData = function() {
+  var counter = 0;
+  zillow.states.forEach(function(element){
+    $.ajax({
+      url: '/zillow/' + element,
+      type: 'GET',
+      // headers: { 'Authorization': 'token ' + githubToken },
+      success: function(data, message, xhr) {
+        zillow.datas.push(data.response.list.region);
+        console.log(zillow.datas);
+        // console.log(zillow.concatArray(zillow.datas));
+      },
+      fail: function(jqXHR, message, error) {
+        console.log(error);
+      }
+    });
+    counter+=1;
+    if (counter === 51) {
+
+      // console.log(zillow.datas);
+      // zillow.filterFunction(zillow.concatArray(zillow.datas));
+      // return zillow.datas;
+    }
+  });
+  // .done(function(){
+  //   zillow.filterFunction(zillow.datas);
+  // });
+};
+
+zillow.concatArray = function(array) {
+  var merged = [].concat.apply([], array);
+  return merged;
+};
+
+zillow.filterFunction = function (array) {
+  var filteredCounties = array.filter(function(element){
+    return element.zindex;
+    // zillow.datas[0].zindex[0]['_']
+  });
+  console.log(filteredCounties);
+  var countyPrices = filteredCounties.map(function(element){
+
+    return element['name'][0] + ' County: Median Home Price is ' + element.zindex[0]['_'];
+  });
+  console.log(countyPrices);
+};
+
 // zillow.requestData();
-
-
-//trulia test curl call
-//http://api.trulia.com/webservices.php?library=TruliaStats&function=getCountyStats&county=Multnomah&state=OR&startDate=2016-05-01&endDate=2016-06-21&apikey=8c2r4kqm6vvq8rdqjv333x7y
