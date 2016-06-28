14bf7472b9736b18a7900dfa073910e94ce9a070
mericks <mericks@users.noreply.github.com>
2016-06-28 16:17:38 -0700

    centered map to fit browser viewport/data overlay

--- js/mapView.js
+++ js/mapView.js
@@ -3,10 +3,10 @@ function initMap(){
   console.log('initMap');
   window.map = new mapboxgl.Map({
     container: 'map', //HTML element to initialize the map in (or element id as string)
-    zoom: 3,
+    zoom: 3.15,
     // minZoom: 1, //Default of 0 (world)
     // maxZoom: 12, //Default of 20 (local)
-    center: [-95.7129, 37.0902], //LatLng array in decimal degrees
+    center: [-80.7129,40.0902], //LatLng array in decimal degrees
     style: 'mapbox://styles/mapbox/outdoors-v9' //Basemap style; can be a preset from mapbox or a user defined style
   });
 
