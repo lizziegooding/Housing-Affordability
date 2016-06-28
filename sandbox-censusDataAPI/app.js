9eb0a8190f16f2818fb0024d9023c2be0f86712a
Michelle Sri <michelle.sri@gmail.com>
2016-06-27 19:00:08 -0700

    sandboxing api calls

--- sandbox-censusDataAPI/app.js
+++ sandbox-censusDataAPI/app.js
@@ -4,14 +4,14 @@ census.enable('ebfc2b4e08bb16ebc53b99604e465907d6d087d5'); // enable the module 
 
 var stateArray = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY'
 ];
+//
+// function populateArr() {
+//   for (var i = 0; i < 4; i++) {
+//     requestArr.push(request + 'i');
+//   }
+// }
 
-function populateArr() {
-  for (var i = 0; i < 4; i++) {
-    requestArr.push(request + 'i');
-  }
-}
-
-var requestArr = [
+var requestObj =
   {
     'level':'state',
     'state':'',
@@ -24,14 +24,34 @@ var requestArr = [
   };
 
   // write a function taht returns the median income and median home value for every county in the usa
+  //
 
+var arr = [];
+function stuff(func){
+  var count = 0;
+  stateArray.forEach(function(ele){
+    requestObj.state = ele;
+    census.APIRequest(requestObj,function(response) {
+      arr.push(response);
+      count++;
+      if(count == stateArray.length){
+        func();
+      }
+    });
+  });
+}
 
-var callback = function(response) {
-  $('#blah').append('<p>' + response.data[0]['commute_time'] + '</p>').text;
-  console.log(response);
-};
+function blah() {
+  console.log(arr);
+}
+//
+// requestArr.forEach(function(element){
+//   census.APIRequest(element,callback);
+//
+// });
 
-requestArr.forEach(function(element){
-  census.APIRequest(element,callback);
-
-});
+stuff(blah);
+// function(response) {
+//   $('#blah').append('<p>' + response.data[0]['commute_time'] + '</p>').text;
+//   console.log(response);
+// };
