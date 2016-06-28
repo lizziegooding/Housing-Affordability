76fd68ab480540b365402673a35006b4f1218100
Michael Freeman <mjfreem1@gmail.com>
2016-06-28 14:52:39 -0700

    got modal and local storage running together and styled modal a bit

---
+++ js/localStorage.js
@@ -0,0 +1,27 @@
+var localStorageAvail = function() {
+  if (typeof(Storage) !== 'undefined') {
+    return true;
+  } else {
+    return false;
+  };
+};
+
+var modalControl = function() {
+  var $instrModal = $('#instrModal');
+  var $checkbox = $('#checkbox');
+  var $agreeButton = $('#agreeButton');
+
+  $agreeButton.on('click', function() {
+    if ($checkbox.prop('checked')) {
+      console.log('reached if statement');
+      localStorage.DoNotShow = 'true';
+    }
+  });
+  if (localStorage.DoNotShow && localStorage.DoNotShow === 'true') {
+    $instrModal.hide();
+  } else {
+    $(window).load(function() {
+      $instrModal.modal('show');
+    });
+  }
+};
