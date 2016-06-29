7fc8f9d48a917bc0f2cab843d783eb980b8be6a6
Aaron Bini <bini.aaron@gmail.com>
2016-06-29 12:00:39 -0700

    incorporating mortgage calc function

--- js/routes.js
+++ js/routes.js
@@ -6,11 +6,12 @@ function setRouteMappings() {
   page('compare', compareController.index);
   page('afford', affordController.index);
   page('about', aboutController.index);
+  page('index.html', initialView.index);
   page('*', function() {
     $('body').html('Sorry! The page you requested doesn\'t exist. Hit your browser\'s "back" button to return to the previous page');
   });
 
-  // page('index.html', '/');
+
 
   page();
 }
