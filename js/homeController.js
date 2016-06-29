43118176bfb16f72f1e5032cad7784bf2c5e801a
mericks <mericks@users.noreply.github.com>
2016-06-29 09:52:13 -0700

    testing tolocale method

--- js/homeController.js
+++ js/homeController.js
@@ -11,6 +11,7 @@
   $('#salarySubmit').on('click', function(e) {
     e.preventDefault();
     var $salaryInput = $('#salaryInput').val();
+    console.log($salaryInput.toLocaleString('en-US'));
     if (isNaN(parseFloat($salaryInput))) {
       console.log('will change class to show user invalid input');
     } else {
