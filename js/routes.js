928c196b10d05dacf1b4ad4621b69c5df9ab43d3
Aaron Bini <bini.aaron@gmail.com>
2016-07-01 09:06:45 -0700

    routing good for now, need to look at it more

--- js/routes.js
+++ js/routes.js
@@ -5,9 +5,9 @@ function setRouteMappings() {
   page('home', homeController.index);
   page('compare', compareController.index);
   page('afford', affordController.index);
-  page('about', aboutController.index);
-  page('funk', funkController.index);
-
+  // page('about', aboutController.index);
+  // page('funk', funkController.index);
+  page('*', homeController.index);
   page('index.html', '/');
 
   page();
